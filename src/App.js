8198ca1bb80a1a756b80b2b5ca2885c4a8c02cf8
Tom van Liempd <tomvanliempd@hotmail.nl>
2021-04-30 12:08:42 +0200

    Added DataUpload functionality (#20)

--- src/App.js
+++ src/App.js
@@ -4,32 +4,34 @@ import Home from './pages/home';
 import About from './pages/about';
 import DataUpload from './pages/DataUpload';
 import Visualize from './pages/visualize';
+import { DataProvider } from './context/data';
 
 function App() {
     return (
-        <Router>
-            {/* Using the Link component, you can create 'buttons' 
+        <DataProvider>
+            <Router>
+                {/* Using the Link component, you can create 'buttons' 
                 to different pagesthat the router can handle.
                 */}
-            {/* Temp max height to simulate ant design navbar that is 64px in height, can be removed */}
-            <nav style={{ maxHeight: '64px' }}>
-                <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/vis'>Visualize</Link>
-                    </li>
-                    <li>
-                          <Link to='/dataUpload'>Data Upload</Link>
-                    </li>
-                </ul>
-            </nav>
+                {/* Temp max height to simulate ant design navbar that is 64px in height, can be removed */}
+                <nav style={{ maxHeight: '64px' }}>
+                    <ul>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>About</Link>
+                        </li>
+                        <li>
+                            <Link to='/vis'>Visualize</Link>
+                        </li>
+                        <li>
+                            <Link to='/dataUpload'>Data Upload</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-            {/* A <Switch> looks through its children <Route>s and
+                {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
                 <Switch>
                     <Route path='/about'>
@@ -45,7 +47,8 @@ function App() {
                         <Home />
                     </Route>
                 </Switch>
-        </Router>        
+            </Router>
+        </DataProvider>  
     );
 }
 
