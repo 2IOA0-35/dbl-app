68beeeb99436eee75ca9cb862be50a59c9449c45
tomvanliempd <tomvanliempd@hotmail.nl>
2021-05-17 16:02:25 +0200

    Added a seperate UserManual component

---
+++ src/pages/visualize/components/UserManual.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CustomMenuItem from './CustomMenuItem';
+
+export default function UserManual() {
+
+    return (
+        <CustomMenuItem>
+            Not yet implemented!
+        </CustomMenuItem>
+    );
+}
